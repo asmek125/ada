79fe4f8334420e9acceaa96c1f421acc9df4c724
asmek125 <96890005+asmek125@users.noreply.github.com>
2022-10-02 21:27:55 +0200

    Test commit

---
+++ src/main/java/simon/ada/App.java
@@ -0,0 +1,15 @@
+package simon.ada;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "ADA!" );
+        
+    }
+}
