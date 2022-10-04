db56c96f5ed9cde80ee133636a1f73245443141d
asmek125 <96890005+asmek125@users.noreply.github.com>
2022-10-04 21:46:11 +0200

    Test

--- src/test/java/simon/ada/AppTest.java
+++ src/test/java/simon/ada/AppTest.java
@@ -16,7 +16,7 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
        
-    	 System.out.println( "Test przeszedł" );
+    	 System.out.println( "Test przeszedł Kurwa jego mać);
     	assertTrue( true );
     }
 }
